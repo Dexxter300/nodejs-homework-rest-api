a5146e06af1ac62d574e81686418524960964f50
Dexxter300 <daniilsafarov2005@gmail.com>
2023-10-21 14:32:36 +0300

    adds database
    saving data in database feature added

---
+++ models/contact.js
@@ -0,0 +1,28 @@
+import { Schema, model } from "mongoose";
+import { handleSaveError, runValidatorsAtUpdate } from "./hooks.js";
+
+const contactSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
+const Contact = model("contact", contactSchema);
+
+contactSchema.post("save", handleSaveError);
+
+contactSchema.pre("findOneAndUpdate", runValidatorsAtUpdate);
+
+contactSchema.post("findOneAndUpdate", handleSaveError);
+
+export default Contact;
